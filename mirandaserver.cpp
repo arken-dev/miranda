b90c8bfc5aa9cecee78af4d9184642cda2f8a321
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2019-04-21 17:35:43 -0300

    refactory miranda

--- mirandaserver.cpp
+++ mirandaserver.cpp
@@ -10,8 +10,6 @@
 #include <QFile>
 #include <QJsonObject>
 
-using charon::service;
-using charon::Log;
 using charon::net::Config;
 
 MirandaServer::MirandaServer(QCoreApplication *app)
@@ -23,45 +21,7 @@ MirandaServer::MirandaServer(QCoreApplication *app)
   m_pid            = config.pid().c_str();
   m_port           = config.port();
   m_maxThreadCount = config.threads();
-  m_service        = config.service();
 
-  QFileInfo dispatch = QFileInfo("dispatch.lua");
-
-  if( ! dispatch.exists() ) {
-    if( app->arguments().size() == 1 ) {
-      qDebug() << "where the dispatch.lua file ???";
-      throw;
-    }
-    QString file_name = app->arguments().at(1);
-    dispatch = QFileInfo(file_name);
-    if( dispatch.exists() ) {
-      qDebug() << "work dir is: " << dispatch.absoluteDir().absolutePath();
-      QDir::setCurrent( dispatch.absoluteDir().absolutePath() );
-    } else {
-      qDebug() << "file dispatch.lua not found exit... ";
-      throw;
-    }
-  }
-
-  // SERVICES
-  if( m_service && os::exists("app/services")) {
-    charon::service::load("app/services");
-  }
-
-  if( os::exists("logs") ) {
-    Log log = Log("logs/miranda.log");
-    log.info("iniciando miranda");
-    if( m_service ) {
-      if( os::exists("app/services") ) {
-        log.info("services started");
-      } else {
-        log.info("services dir not exists");
-      }
-    } else {
-      log.info("services not enable");
-    }
-    log.dump();
-  }
 
 }
 
